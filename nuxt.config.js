894613ebf8591e6540ffb14d131e790068e05a9d
AlphaActual <tin.priti@gmail.com>
2023-03-24 22:33:53 +0100

    Setup mape i leafleta

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,10 +13,12 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css", rel: "stylesheet", integrity: "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC", crossorigin: "anonymous" }
+      { href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css", rel: "stylesheet", integrity: "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC", crossorigin: "anonymous" },
+      { rel: "stylesheet", href: "https://unpkg.com/leaflet@1.9.3/dist/leaflet.css", integrity: "sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=", crossorigin: "" }
     ],
     script: [
-      { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js", integrity: "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM", crossorigin: "anonymous" }
+      { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js", integrity: "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM", crossorigin: "anonymous" },
+      { src: "https://unpkg.com/leaflet@1.9.3/dist/leaflet.js", integrity: "sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=", crossorigin: "" }
     ]
   },
 
@@ -25,7 +27,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
+  plugins: [{ src: '~/plugins/leaflet.js', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -38,7 +40,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    'bootstrap-vue/nuxt', 'nuxt-leaflet'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,57 @@
 <template>
-  <h1 class="display-1 text-primary">Hello world</h1>
+  <div class="container-fluid">
+    <div class="row">
+      <aside id="side-panel" class="col-3">
+        <h2>Podaci</h2>
+        <div v-for="(wp,i) in waypoints" :key="i">
+          ID: {{wp.id}} Lat:{{ wp.lat }} Long: {{ wp.lng }}
+        </div>
+      </aside>
+      <div class="col-9" id="map-wrap" >
+        <client-only>
+          <l-map :zoom="zoom" :center="center" @click="setmarker">
+            <l-tile-layer :url="url"></l-tile-layer>
+            <l-marker v-for="(wp,i) in waypoints" :lat-lng="[wp.lat,wp.lng]" :key="i"></l-marker>
+            <!-- <l-polyline ></l-polyline> -->
+          </l-map>
+        </client-only>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      zoom: 13,
+      center: [47.3686498, 8.5391825],
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      marker: [47.3686498, 8.5391825],
+      waypoints: []
+
+    }
+  },
+  methods: {
+    setmarker(e){
+      console.dir(e);
+      let latitude = e.latlng.lat;
+      let longitude = e.latlng.lng;
+      console.dir(latitude,longitude);
+      if(e.originalEvent.target.alt != "Marker"){
+
+        this.waypoints = [...this.waypoints,{id:this.waypoints.length,lat:latitude,lng:longitude}]
+      }
+    }
+
+  }
 }
 </script>
+<style scoped>
+  #map-wrap {
+    height: 100vh;
+    width: 100%;
+  }
+</style>
