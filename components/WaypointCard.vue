b1f1c6586231aeb15a2c19a154031e73bcacf84c
AlphaActual <tin.priti@gmail.com>
2023-03-25 12:49:10 +0100

    dodana card komponenta

---
+++ components/WaypointCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="card mb-2 p-2">
+          Name: WP{{ waypoint.name }} <br>
+          ID: {{waypoint.id}} <br>
+          Lat: {{ formatLatitude(waypoint.lat) }} <br>
+          Long: {{ formatLongitude(waypoint.lng) }}<br>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+  name: 'WaypointCard',
+  props: {  
+    waypoint:{
+        type: Object,
+        required:true
+    }
+
+  },
+  methods: {
+    formatLatitude(latitude) {
+      let absolute = Math.abs(latitude); // Get absolute value of latitude
+      let degrees = Math.floor(absolute); // Get whole degrees
+      let minutes = Math.floor((absolute - degrees) * 60); // Get whole minutes
+      let seconds = ((absolute - degrees - minutes / 60) * 3600).toFixed(2); // Get seconds and round to 2 decimal places
+      let direction = latitude >= 0 ? "N" : "S"; // Determine N or S
+      
+      return degrees + "° " + minutes + "' " + seconds + "\" " + direction;
+    },
+    formatLongitude(longitude) {
+      let absolute = Math.abs(longitude); // Get absolute value of longitude
+      let degrees = Math.floor(absolute); // Get whole degrees
+      let minutes = Math.floor((absolute - degrees) * 60); // Get whole minutes
+      let seconds = ((absolute - degrees - minutes / 60) * 3600).toFixed(2); // Get seconds and round to 2 decimal places
+      let direction = longitude >= 0 ? "E" : "W"; // Determine E or W
+      
+      return degrees + "° " + minutes + "' " + seconds + "\" " + direction;
+    }
+  }
+    
+};
+
+</script>
+
+<style scoped>
+   
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,25 @@
 <template>
-  <div class="main-container">
-    <div class="d-flex wrapper">
+  <div class="container-fluid">
+    <div class="row wrapper">
 
-      <aside id="side-panel" class="p-4">
+      <aside id="side-panel" class="p-4 col-3">
         <h2>Podaci</h2>
         <div class="form-check form-switch">
           <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
           <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
         </div>
         <hr>
-        <div v-for="(wp,i) in waypoints" :key="i">
-          ID: {{wp.id}} Lat:{{ wp.lat }} Long: {{ wp.lng }}
-        </div>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos doloribus deleniti neque facere tenetur fuga nostrum expedita repellendus. Iusto illum iste exercitationem voluptatibus eligendi ipsa omnis similique asperiores eius deleniti, officia sit minima sunt ex doloremque vero natus culpa beatae temporibus cupiditate unde mollitia dolores tenetur. Autem dolorum sapiente, sit obcaecati numquam iusto laboriosam nisi. Quae mollitia commodi nam placeat excepturi tempore porro quis, eaque, itaque fugiat aperiam officia beatae sunt, dignissimos corrupti quisquam incidunt vel. Perspiciatis, nesciunt reprehenderit sequi dicta, praesentium provident architecto quod excepturi autem sunt eos asperiores vero facere! Dolorem perspiciatis ea eligendi ex nisi cupiditate!</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos doloribus deleniti neque facere tenetur fuga nostrum expedita repellendus. Iusto illum iste exercitationem voluptatibus eligendi ipsa omnis similique asperiores eius deleniti, officia sit minima sunt ex doloremque vero natus culpa beatae temporibus cupiditate unde mollitia dolores tenetur. Autem dolorum sapiente, sit obcaecati numquam iusto laboriosam nisi. Quae mollitia commodi nam placeat excepturi tempore porro quis, eaque, itaque fugiat aperiam officia beatae sunt, dignissimos corrupti quisquam incidunt vel. Perspiciatis, nesciunt reprehenderit sequi dicta, praesentium provident architecto quod excepturi autem sunt eos asperiores vero facere! Dolorem perspiciatis ea eligendi ex nisi cupiditate!</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos doloribus deleniti neque facere tenetur fuga nostrum expedita repellendus. Iusto illum iste exercitationem voluptatibus eligendi ipsa omnis similique asperiores eius deleniti, officia sit minima sunt ex doloremque vero natus culpa beatae temporibus cupiditate unde mollitia dolores tenetur. Autem dolorum sapiente, sit obcaecati numquam iusto laboriosam nisi. Quae mollitia commodi nam placeat excepturi tempore porro quis, eaque, itaque fugiat aperiam officia beatae sunt, dignissimos corrupti quisquam incidunt vel. Perspiciatis, nesciunt reprehenderit sequi dicta, praesentium provident architecto quod excepturi autem sunt eos asperiores vero facere! Dolorem perspiciatis ea eligendi ex nisi cupiditate!</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quos doloribus deleniti neque facere tenetur fuga nostrum expedita repellendus. Iusto illum iste exercitationem voluptatibus eligendi ipsa omnis similique asperiores eius deleniti, officia sit minima sunt ex doloremque vero natus culpa beatae temporibus cupiditate unde mollitia dolores tenetur. Autem dolorum sapiente, sit obcaecati numquam iusto laboriosam nisi. Quae mollitia commodi nam placeat excepturi tempore porro quis, eaque, itaque fugiat aperiam officia beatae sunt, dignissimos corrupti quisquam incidunt vel. Perspiciatis, nesciunt reprehenderit sequi dicta, praesentium provident architecto quod excepturi autem sunt eos asperiores vero facere! Dolorem perspiciatis ea eligendi ex nisi cupiditate!</p>
+        <!-- waypoint list -->
+        <WaypointCard v-for="(wp) in waypoints" :waypoint="wp" :key="wp.id" />
+          
       </aside>
 
-      <div id="map-wrap" >
+      <div id="map-wrap" class="col-9" >
         <client-only>
           <l-map :zoom="zoom" :center="center" @click="setmarker">
             <l-tile-layer :url="url"></l-tile-layer>
-            <l-marker v-for="(wp,i) in waypoints" :lat-lng="[wp.lat,wp.lng]" :key="i"></l-marker>
-            <!-- <l-polyline ></l-polyline> -->
+            <l-marker v-for="(wp) in waypoints" :lat-lng="[wp.lat,wp.lng]" :key="wp.id"></l-marker>
+            <l-polyline ></l-polyline>
           </l-map>
         </client-only>
       </div>
@@ -46,13 +42,26 @@ export default {
     }
   },
   methods: {
+    updateNames(){
+      // fix ovdje da bude reversed!
+      this.waypoints = this.waypoints.map((wp,i)=>{return {...wp,name:i}});
+    },
     setmarker(e){
       console.dir(e);
       let latitude = e.latlng.lat;
       let longitude = e.latlng.lng;
       // ako nije kliknuto na marker spremi novi waypoint u listu waypoints
       if(e.originalEvent.target.alt != "Marker"){
-        this.waypoints = [...this.waypoints,{id:this.waypoints.length,lat:latitude,lng:longitude}]
+        const newWaypoint = {
+          id: Date.now(),
+          name: 0,
+          lat: latitude,
+          lng: longitude
+        };
+        // update array
+        this.waypoints = [newWaypoint,...this.waypoints];
+        // podesi imena (ako imamo wp1,wp2,wp3 i obrisemo wp2, potrebno je svima podesiti imena tako da se wp3 sada zove wp2)
+        this.updateNames();
       }
     }
 
@@ -68,15 +77,11 @@ export default {
     overflow-y: hidden;
   }
   #map-wrap {
-    /* position: fixed;
-    right:0px; */
     height: 100vh;
-    width: 70%;
     overflow: hidden;
   }
   #side-panel{
-    width: 30%;
     height: calc(100vh);
-    overflow-y: scroll;
+    overflow-y: auto;
   }
 </style>
