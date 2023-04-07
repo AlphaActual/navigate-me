0f17f5ac939447cd05940c9c3e4820dd4a92f758
AlphaActual <tin.priti@gmail.com>
2023-04-07 19:12:26 +0200

    dodane nove animacije

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,7 @@
 <div class="container">
   <div class="row justify-content-center align-items-center">
     <div class="col">
-      <p class="display-6">Tu ce biti link na login registraciju</p>
-      <img src="../assets/img/icons/start.png" alt="">
+      <img class="img-fluid spinning-wheel" src="../assets/img/compass.svg" alt="">
     </div>
     <div class="col">
       <nuxt-link to="/add-edit-route"><button class="btn btn-outline-success">Dodaj rutu</button></nuxt-link>
@@ -18,3 +17,58 @@ export default {
   name:'Main page'
 }
 </script>
+
+<style scoped>
+  .row {
+    height: 100vh;
+  }
+
+  .spinning-wheel {
+  animation: spin 5s infinite;
+  animation-fill-mode: infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+    animation-timing-function: ease-out;
+  }
+  20% {
+    transform: rotate(270deg);
+    animation-timing-function: ease-in;
+  }
+  30% {
+    transform: rotate(270deg);
+    animation-timing-function: ease-in;
+  }
+  40% {
+    transform: rotate(270deg);
+    animation-timing-function: ease-in;
+  }
+  50% {
+    transform: rotate(270deg);
+    animation-timing-function: ease-in;
+  }
+  60% {
+    transform: rotate(-180deg);
+    animation-timing-function: ease-out;
+  }
+  70% {
+    transform: rotate(-180deg);
+    animation-timing-function: ease-out;
+  }
+  80% {
+    transform: rotate(-180deg);
+    animation-timing-function: ease-out;
+  }
+  90% {
+    transform: rotate(-180deg);
+    animation-timing-function: ease-out;
+  }
+  100% {
+    transform: rotate(360deg);
+    animation-timing-function: ease-in;
+  }
+}
+
+</style>
