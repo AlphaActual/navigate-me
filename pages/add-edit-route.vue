c34ade7dd760c7249494c75b1143ddce466dac77
AlphaActual <tin.priti@gmail.com>
2023-04-10 15:41:35 +0200

    dodani novi markeri i circle

--- pages/add-edit-route.vue
+++ pages/add-edit-route.vue
@@ -5,51 +5,88 @@
     <div class="row wrapper">
 
       <aside v-in-viewport id="side-panel" class="ps-4 pt-4 pb-4 slide-left">
+        <!-- save row -->
         <div>
           <div>
             <button class="save-btn btn d-block mb-2">Save route</button>
             <input v-model="routeName" name="name-input" id="name-input" type="text" class="text-center btn-secondary-outline p-1 d-block"/>
           </div>
         </div>
+        <!-- end of save row -->
 
         <hr>
 
+        <!-- speed row -->
          <div>
           <div class="d-flex align-items-center">
-            <label class="mb-0 me-2" for="speed-input">Set speed: </label>
-            <input v-model="shipSpeed" name="speed-input" id="speed-input" type="number" min="0.1" step="0.1" required class="text-danger-main fw-bold btn-secondary-outline d-block"/> kts
-            <button @click="updateSpeeds" class=" animated-button btn ms-1"><i class=" icon-1 fa-solid fa-play"></i><i class=" icon-2 fa-solid fa-play"></i><i class=" icon-3 fa-solid fa-play"></i></button>
-            <!-- <button @click="updateSpeeds" class="btn btn-outline-primary ms-1">&#8594;</button> -->
+            <label class="mb-0 me-2" for="speed-input">Set {{circleChecked ? 'radius':'speed'}}: </label>
+            <input v-model="shipSpeed" name="speed-input" id="speed-input" type="number" min="0.1" step="0.1" required class="text-danger-main fw-bold btn-secondary-outline d-block"/> {{circleChecked ? 'NM':'kts'}}
+            <button v-if="!circleChecked" @click="updateSpeeds" class=" animated-button btn ms-1"><i class=" icon-1 fa-solid fa-play"></i><i class=" icon-2 fa-solid fa-play"></i><i class=" icon-3 fa-solid fa-play"></i></button>
           </div>
         </div>
+        <!-- end of speed row -->
 
         <hr>
 
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="d-flex align-items-center">
-              <button class=" sort-button btn btn-outline-secondary" @click="sortWPs">Sort <i :class="['fa-solid', 'fa-arrow-down',{ 'reverse-arrow': !sortedDescending }]"></i></button>
-              <!-- <span :class="['sort-arrow', 'fs-3', 'ms-1',{ 'reverse-arrow': !sortedDescending } ]">&#8595;</span> -->
+        <!-- marker icons row -->
+        <div class="icons-row d-flex justify-content-around align-items-center">
+          
+            <input type="radio" class="btn-check" name="options" id="option-waypoint" autocomplete="off" checked>
+            <label @click="CircleChecked(false)" class="btn btn-warning" for="option-waypoint"><i class="fa-solid fa-location-dot"></i></label>
+
+            <input type="radio" class="btn-check" name="options" id="option-circle" autocomplete="off">
+            <label @click="CircleChecked(true)" class="btn btn-warning" for="option-circle"><i class="fa-regular fa-circle"></i></label>
+
+            <input type="radio" class="btn-check" name="options" id="option-anchor" autocomplete="off">
+            <label @click="CircleChecked(false)" class="btn btn-warning" for="option-anchor"><i class="fa-solid fa-anchor"></i></label>
+
+            <input type="radio" class="btn-check" name="options" id="option-pin" autocomplete="off">
+            <label @click="CircleChecked(false)" class="btn btn-warning" for="option-pin"><i class="fa-solid fa-map-pin"></i></label>
+
+            <div class="marker-info">
+              <input v-model="markerDescription" name="marker-in
+              circleChecked: false,put" id="marker-input" type="text" class="btn-secondary-outline p-1 d-block"/>
             </div>
 
-            <div>
-              <div class="active-wp btn rounded" @click="centerOnActive">Active: <span class="fw-bold text-danger-main">{{activeWP?.name}}</span></div>
-              <div @click="insertWp" :class="['btn', 'btn-warning',{'disabled': this.activeWP?.id === this.waypoints[this.waypoints.length-1]?.id}]">Insert</div>
-              <div @click="removeWP(activeWP?.id)" class="btn btn-danger">Delete active</div>
-            </div>
+          
+        </div>
+        <!--end of marker icons row -->
+
+        <hr>
+
+        <!-- sort active insert delete row -->
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="d-flex align-items-center">
+            <button class=" sort-button btn btn-outline-secondary" @click="sortWPs">Sort <i :class="['fa-solid', 'fa-arrow-down',{ 'reverse-arrow': !sortedDescending }]"></i></button>
+            <!-- <span :class="['sort-arrow', 'fs-3', 'ms-1',{ 'reverse-arrow': !sortedDescending } ]">&#8595;</span> -->
           </div>
+
+          <div>
+            <div class="active-wp btn rounded" @click="centerOnActive">Active: <span class="fw-bold text-danger-main">{{activeWP?.name}}</span></div>
+            <div @click="insertWp" :class="['btn', 'btn-warning',{'disabled': this.activeWP?.id === this.waypoints[this.waypoints.length-1]?.id}]">Insert</div>
+            <div @click="removeWP(activeWP?.id)" class="btn btn-danger">Delete active</div>
+          </div>
+        </div>
+        <!-- end of sort active insert delete row -->
+
         <hr>
 
+        <!-- waypoint cards -->
         <div class="waypoint-container">
           <!-- waypoint list -->
           <WaypointCard v-in-viewport.once class="slide-left" :lastWp="waypoints[waypoints.length-1]" :activePoint="activeWP" v-for="(wp) in showSorted" :waypoint="wp" :key="wp.id" @card-clicked="handleCardClick(wp)"/>
         </div>
+        <!-- end of waypoint cards -->
       </aside>
 
       <div id="map-wrap" >
         <client-only>
+          <!-- map -->
           <l-map :zoom="zoom" :center="center" @click="handleMapClick">
+            <!-- tile layer -->
             <l-tile-layer :url="url"></l-tile-layer>
-            <!-- <l-image-overlay :url="overlayUrl" :bounds="bounds"></l-image-overlay> -->
+            
+            <!-- waypoint markers -->
             <l-marker v-for="(wp,index) in waypoints" :lat-lng="[wp.lat,wp.lng]" @click="markerClick(wp.id)" :key="wp.id" :icon="index === 0 ? firstMarkerIcon : defaultMarkerIcon" >
               <l-popup> 
                 <p class="p-0 m-0 fw-bold">WP{{wp.name}}</p> 
@@ -58,7 +95,38 @@
                 <p class="p-0 m-0">Speed: {{wp.id != waypoints[waypoints.length-1].id ? wp.speed + ' kts':'N/A'}}</p>
                 </l-popup>
             </l-marker>
+            <!-- polylines -->
             <l-polyline :lat-lngs="getLinesCoordinates" ></l-polyline>
+
+            <!-- circle markers -->
+            <l-circle v-for="(circle) in circles" :key="circle.id" :lat-lng="[circle.lat, circle.lng]" :radius="circle.radius">
+              <l-popup>
+                <p class="p-0 m-0 fw-bold">{{circle.description}}</p>
+                <p class="p-0 m-0">Radius: {{circle.radius/1852}} NM</p>
+                <button @click="removeCircle(circle.id)" class=" btn btn-outline-danger ps-2 pe-2 pt-0 pb-0">Delete</button>
+              </l-popup>
+            </l-circle>
+
+            <!-- anchor markers -->
+            <l-marker v-for="(anchor) in anchors" :key="anchor.id" :lat-lng="[anchor.lat, anchor.lng]" :icon="anchorMarkerIcon">
+              <l-popup>
+                <p class="p-0 m-0 fw-bold">{{anchor.description}}</p>
+                <p class="p-0 m-0">Lat: {{formatLatitude(anchor.lat)}}</p>
+                <p class="p-0 m-0">Long: {{formatLongitude(anchor.lng)}}</p>
+                <button @click="removeAnchor(anchor.id)" class=" btn btn-outline-danger ps-2 pe-2 pt-0 pb-0">Delete</button>
+              </l-popup>
+            </l-marker>
+
+            <!-- pin markers -->
+            <l-marker v-for="(pin) in pins" :key="pin.id" :lat-lng="[pin.lat, pin.lng]" :icon="pinMarkerIcon">
+              <l-popup>
+                <p class="p-0 m-0 fw-bold">{{pin.description}}</p>
+                <p class="p-0 m-0">Lat: {{formatLatitude(pin.lat)}}</p>
+                <p class="p-0 m-0">Long: {{formatLongitude(pin.lng)}}</p>
+                <button @click="removePin(pin.id)" class=" btn btn-outline-danger ps-2 pe-2 pt-0 pb-0">Delete</button>
+              </l-popup>
+            </l-marker>
+
           </l-map>
         </client-only>
         <div class="line line-vertical"></div>
@@ -90,11 +158,16 @@ export default {
       // bounds: [[45.5727, 12.9830], [44.924946, 12.363739]],
       marker: [45.08397548484512, 13.633303642272951],
       waypoints: [],
+      circles:[],
+      anchors:[],
+      pins:[],
       sortedDescending: true,
       activeWP: {id:0},
       shipSpeed: 7,
       totalDistance: 0,
       totalTimeHrs:0,
+      markerDescription: 'MyMarkerName',
+      circleChecked: false,
       firstMarkerIcon: L.icon({
                 iconUrl: require('@/assets/img/icons/start.png'),
                 iconSize: [41, 41],
@@ -108,7 +181,21 @@ export default {
                 iconAnchor: [12, 41],
                 popupAnchor: [1, -34],
                 tooltipAnchor: [16, -28],
-                })
+                }),
+      anchorMarkerIcon: L.icon({
+                iconUrl: require('@/assets/img/icons/anchor.svg'),
+                iconSize: [41, 41],
+                iconAnchor: [20, 20],
+                popupAnchor: [1, -34],
+                tooltipAnchor: [16, -28],
+                }),
+      pinMarkerIcon: L.icon({
+                iconUrl: require('@/assets/img/icons/pin.svg'),
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                tooltipAnchor: [16, -28],
+                }),
     }
   },
   watch:{
@@ -134,6 +221,9 @@ export default {
     setActiveWP(wp){
       this.activeWP = wp;
     },
+    CircleChecked(val){
+      this.circleChecked = val;
+    },
     markerClick(id){
       const updatedNewWPindex = this.waypoints.findIndex(wp=>wp.id === id);
         // postavi ga kao aktivnog
@@ -153,7 +243,23 @@ export default {
     },
     handleMapClick(e){
       if(e.originalEvent.target.alt != "Marker"){
-        this.setmarker(e);
+        // ako je wp
+        if(document.getElementById('option-waypoint').checked){
+          this.setWaypoint(e);
+        }
+
+        // ako je circle
+        else if(document.getElementById('option-circle').checked){
+          this.setCircle(e);
+        }
+        // ako je anchor
+        else if(document.getElementById('option-anchor').checked){
+          this.setAnchor(e);
+        }
+        // ako je pin
+        else if(document.getElementById('option-pin').checked){
+          this.setPin(e);
+        }
       };
     },
     handleCardClick(wp){
@@ -211,7 +317,7 @@ export default {
       this.centerOnActive();
 
     },
-    setmarker(e){
+    setWaypoint(e){
       let latitude = e.latlng.lat;
       let longitude = e.latlng.lng;
       let datum = new Date;
@@ -268,7 +374,78 @@ export default {
         }
       }
     }, 
+    setCircle(e){
+      let latitude = e.latlng.lat;
+      let longitude = e.latlng.lng;
+      let datum = new Date;
+      
+      const newCircle = {
+        id: Date.now(),
+        description: this.markerDescription,
+        lat: latitude,
+        lng: longitude,
+        radius: this.shipSpeed * 1852, 
+        timeCreated: `${datum.getFullYear()}-${String(datum.getMonth()+1).padStart(2, '0')}-${String(datum.getDate()).padStart(2, '0')} ${String(datum.getHours()).padStart(2, '0')}:${String(datum.getMinutes()).padStart(2, '0')}:${String(datum.getSeconds()).padStart(2, '0')}`
+      };
+      
+
+      this.circles = [...this.circles,newCircle];
+    },
+    removeCircle(id){
+      if(id){
+        
+        // izbaci ga iz circles liste
+        this.circles = this.circles.filter(wp=>wp.id !== id)
+      }
+    },
+    setAnchor(e){
+      let latitude = e.latlng.lat;
+      let longitude = e.latlng.lng;
+      let datum = new Date;
+      
+      const newAnchor = {
+        id: Date.now(),
+        description: this.markerDescription,
+        lat: latitude,
+        lng: longitude,
+        timeCreated: `${datum.getFullYear()}-${String(datum.getMonth()+1).padStart(2, '0')}-${String(datum.getDate()).padStart(2, '0')} ${String(datum.getHours()).padStart(2, '0')}:${String(datum.getMinutes()).padStart(2, '0')}:${String(datum.getSeconds()).padStart(2, '0')}`
+      };
+      
+
+      this.anchors = [...this.anchors,newAnchor];
+    },
+    removeAnchor(id){
+      if(id){
+        
+        // izbaci ga iz anchors liste
+        this.anchors = this.anchors.filter(wp=>wp.id !== id)
+      }
+    },
+    setPin(e){
+      let latitude = e.latlng.lat;
+      let longitude = e.latlng.lng;
+      let datum = new Date;
+      
+      const newPin = {
+        id: Date.now(),
+        description: this.markerDescription,
+        lat: latitude,
+        lng: longitude,
+        timeCreated: `${datum.getFullYear()}-${String(datum.getMonth()+1).padStart(2, '0')}-${String(datum.getDate()).padStart(2, '0')} ${String(datum.getHours()).padStart(2, '0')}:${String(datum.getMinutes()).padStart(2, '0')}:${String(datum.getSeconds()).padStart(2, '0')}`
+      };
+      
+
+      this.pins = [...this.pins,newPin];
+    },
+    removePin(id){
+      if(id){
+        
+        // izbaci ga iz pins liste
+        this.pins = this.pins.filter(wp=>wp.id !== id)
+      }
+    },
     updateSpeeds(){
+      if(this.waypoints.length < 1) return;
       // pronadji indeks aktivnog waypointa
       const activeIndex = this.waypoints.findIndex(wp=>wp.id === this.activeWP.id)
       // updejtaj speed za sve waypointove od aktivnog pa do kraja arraya
@@ -352,6 +529,30 @@ export default {
         this.getTotalDistance();
         this.getTotalTime();
       }
+    },
+    formatLatitude(latitude) {
+      let absolute = Math.abs(latitude); // Get absolute value of latitude
+      let degrees = Math.floor(absolute); // Get whole degrees
+      let minutes = Math.floor((absolute - degrees) * 60); // Get whole minutes
+      let seconds = ((absolute - degrees - minutes / 60) * 3600).toFixed(2); // Get seconds and round to 2 decimal places
+      let direction = latitude >= 0 ? "N" : "S"; // Determine N or S
+
+      degrees = String(degrees).padStart(3, '0');
+      minutes = String(minutes).padStart(2, '0');
+
+      return degrees + "° " + minutes + "' " + seconds + "\" " + direction;
+    },
+    formatLongitude(longitude) {
+      let absolute = Math.abs(longitude); // Get absolute value of longitude
+      let degrees = Math.floor(absolute); // Get whole degrees
+      let minutes = Math.floor((absolute - degrees) * 60); // Get whole minutes
+      let seconds = ((absolute - degrees - minutes / 60) * 3600).toFixed(2); // Get seconds and round to 2 decimal places
+      let direction = longitude >= 0 ? "E" : "W"; // Determine E or W
+
+      degrees = String(degrees).padStart(3, '0');
+      minutes = String(minutes).padStart(2, '0');
+
+      return degrees + "° " + minutes + "' " + seconds + "\" " + direction;
     }
   }
 }
@@ -396,6 +597,22 @@ export default {
   #side-panel{
     height: 100%;
     /* overflow-y: scroll; */
+  }
+  .icons-row .btn {
+    margin-bottom:0;
+    margin-right: 4px;
+  }
+  .icons-row input:checked + label {
+    background-color: var(--brown-main);
+    color: var(--warning-main);
+  }
+ 
+  .marker-info{
+    width: 100%;
+    padding-left: 14px;
+  }
+  #marker-input {
+    width: 100%;
   }
   .waypoint-container {
     overflow: auto;
