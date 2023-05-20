9f86ec5f9cdc4f352fd845b1665deb7819328051
NevenDavidovic <nenoronnie@gmmail.com>
2023-05-20 18:01:39 +0200

    register button

--- pages/index.vue
+++ pages/index.vue
@@ -2,55 +2,82 @@
   <div class="container-fluid">
     <div class="row align-items-center">
       <div class="col-5 image-col">
-        <img class="img-fluid compass" src="../assets/img/kompas.svg" alt="compass">  
+        <img
+          class="img-fluid compass"
+          src="../assets/img/kompas.svg"
+          alt="compass"
+        />
       </div>
-      
+
       <div class="col">
         <h1 class="main-title display-3">NAVIGATE ME</h1>
         <div v-in-viewport class="form-container slide">
           <form>
             <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              <label for="exampleInputEmail1" class="form-label"
+                >Email address</label
+              >
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" class="form-text">
+                We'll never share your email with anyone else.
+              </div>
             </div>
             <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
+              <label for="exampleInputPassword1" class="form-label"
+                >Password</label
+              >
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+              />
             </div>
-            <button type="button" class="login-button btn btn-outline-primary">Login</button>
+            <button type="button" class="login-button btn btn-outline-primary">
+              Login
+            </button>
           </form>
 
-          <div style="margin-top: 30px;">
+          <div style="margin-top: 30px">
+            <button ype="button" class="login-button btn btn-outline-primary">
+              <nuxt-link to="/register" class="no-underline">
+                Register
+              </nuxt-link>
+            </button>
+          </div>
+
+          <div style="margin-top: 30px">
             <nuxt-link to="/routes">Skip login(for testing)</nuxt-link>
           </div>
-          
         </div>
-  
-        <img class="path-image" src="../assets/img/path.svg" alt="path">
-      </div>
 
+        <img class="path-image" src="../assets/img/path.svg" alt="path" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 // za koristenje v-in-viewport
-import Vue from 'vue'
-import inViewportDirective from 'vue-in-viewport-directive'
-Vue.directive('in-viewport', inViewportDirective)
+import Vue from "vue";
+import inViewportDirective from "vue-in-viewport-directive";
+Vue.directive("in-viewport", inViewportDirective);
 
 export default {
-  name:'Login',
-  transition: 'fade',
-  
-  mounted(){
-    const compass = document.querySelector('.compass');
+  name: "Login",
+  transition: "fade",
+
+  mounted() {
+    const compass = document.querySelector(".compass");
     let mouseX = 0;
     let mouseY = 0;
-    document.addEventListener('mousemove', event => {
-    mouseX = event.clientX;
-    mouseY = event.clientY;
+    document.addEventListener("mousemove", (event) => {
+      mouseX = event.clientX;
+      mouseY = event.clientY;
     });
     function updateCompass() {
       const rect = compass.getBoundingClientRect();
@@ -59,7 +86,8 @@ export default {
       const deltaX = mouseX - centerX;
       const deltaY = mouseY - centerY;
       const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-      let targetAngle = (Math.atan2(deltaY, deltaX) * 180 / Math.PI + 360) % 360;
+      let targetAngle =
+        ((Math.atan2(deltaY, deltaX) * 180) / Math.PI + 360) % 360;
       const transformValues = compass.style.transform.split(/\w+\(|\);?/);
       const currentAngle = parseFloat(transformValues[1]) || 0;
       let angleDiff = targetAngle - currentAngle;
@@ -71,75 +99,69 @@ export default {
       }
 
       targetAngle = currentAngle + angleDiff;
-      const rotation = `rotateZ(${targetAngle +90}deg)`;
-      
+      const rotation = `rotateZ(${targetAngle + 90}deg)`;
+
       if (distance < 100) {
         compass.style.transition = `transform .066s ease-in-out`;
       } else {
-        compass.style.transition = `transform ${distance/1500}s ease-in-out`;        
+        compass.style.transition = `transform ${distance / 1500}s ease-in-out`;
       }
 
       compass.style.transform = `${rotation}`;
     }
 
     setInterval(updateCompass, 16);
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-
-  @import url('https://fonts.googleapis.com/css2?family=Cinzel&family=Open+Sans&display=swap');
-  @import '~/assets/css/main.css';
+@import url("https://fonts.googleapis.com/css2?family=Cinzel&family=Open+Sans&display=swap");
+@import "~/assets/css/main.css";
+.main-title {
+  font-family: "Cinzel", "Times New Roman", Times, serif, sans-serif;
+  color: var(--brown-main);
+}
+@media screen and (max-width: 1500px) {
   .main-title {
-    font-family: 'Cinzel','Times New Roman', Times, serif, sans-serif;
-    color: var(--brown-main);
+    font-size: 3.5rem;
   }
-  @media screen and (max-width: 1500px) {
-    .main-title {
-      font-size: 3.5rem;
-    }
-  }
-    
-  
-  .container-fluid {
-    /* background: url('~assets/img/chart-bg.png'); */
-    /* background-color: inherit; */
-    background-size: cover;
-    background-position: center;
-    
-  }
-  .path-image {
-    width: 20vw;
-    max-width: 300px;
-    position: absolute;
-    right:13%;
-    bottom:-41%;
-    
+}
 
-  }
-  .image-col {
-    overflow: hidden;
-  }
-  /* .image-col > img {
+.container-fluid {
+  /* background: url('~assets/img/chart-bg.png'); */
+  /* background-color: inherit; */
+  background-size: cover;
+  background-position: center;
+}
+.path-image {
+  width: 20vw;
+  max-width: 300px;
+  position: absolute;
+  right: 13%;
+  bottom: -41%;
+}
+.image-col {
+  overflow: hidden;
+}
+/* .image-col > img {
     max-width: 80%;
   } */
-  .row {
-    height: 100vh;
-  }
-  .form-container{
-    width: 50%;
-  }
-  .login-button {
-    display: block;
-    width: 100%;
-    text-align: center;
-    color:black;
-    border-color:var(--brown-main);
-  }
-  .login-button:hover {
-    background-color: var(--brown-main);
-    color: white;
-  }
-  
+.row {
+  height: 100vh;
+}
+.form-container {
+  width: 50%;
+}
+.login-button {
+  display: block;
+  width: 100%;
+  text-align: center;
+  color: black;
+  border-color: var(--brown-main);
+}
+.login-button:hover {
+  background-color: var(--brown-main);
+  color: white;
+}
 </style>
